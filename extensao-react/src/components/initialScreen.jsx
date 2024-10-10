134d55b494112ca942a3affd7e96c92fe599dd27
pedro00876 <pcchagas00@gmail.com>
2024-10-10 15:23:28 -0300

    Adicionando os conteúdos da extensão

--- extensao-react/src/components/initialScreen.jsx
+++ extensao-react/src/components/initialScreen.jsx
@@ -3,7 +3,45 @@ import "./InitialScreen.css";
 const InitialScreen = () => {
   return (
     <div>
-      <h1 className="title">JetSales</h1>
+      <div>
+        <header>
+          <h2>User:</h2>
+          <button>Carteira Sigma</button>
+          <button>Manutenção</button>
+        </header>
+      </div>
+      <main>
+        <div>
+          <section>
+            <h3>Dados do Cliente</h3>
+          </section>
+        </div>
+        <div>
+          <section>
+            <h3>Descrição</h3>
+          </section>
+        </div>
+        <div>
+          <section>
+            <h3>Plataformas</h3>
+          </section>
+        </div>
+        <div>
+          <section>
+            <h3>Plano</h3>
+          </section>
+        </div>
+        <div>
+          <section>
+            <h3>Dados financeiros</h3>
+          </section>
+        </div>
+      </main>
+      <div>
+        <footer>
+          <button>Sigma</button>
+        </footer>
+      </div>
     </div>
   );
 };
